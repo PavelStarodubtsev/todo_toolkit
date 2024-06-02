b7d3af93d387f01536a50b1d9b3011ce938c7923
PavelStar <pvstarodubtsev@gmail.com>
2024-06-02 16:50:08 +0300

    release 2

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteTodo, switchTodoComplete } from '../Redux/slices/todoSlice';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteTodo, switchTodoComplete } from "../Redux/slices/todoSlice";
+import "./style.css";
 
-export default function TodoItem({
-  id, text, complited,
-}) {
-  const dispatch = useDispatch();
+export default function TodoItem({ id, text, complited }) {
+    const dispatch = useDispatch();
 
-  return (
-    <li>
-      <input type="checkbox" checked={complited} onChange={() => dispatch(switchTodoComplete({ id }))} />
-      <span>{text}</span>
+    return (
+        <li>
+            <input
+                type="checkbox"
+                checked={complited}
+                onChange={() => dispatch(switchTodoComplete({ id }))}
+            />
 
-      <button
-        type="button"
-        onClick={() => dispatch(deleteTodo({ id }))}
-        style={{ color: 'red', marginLeft: '5px', cursor: 'pointer' }}
-      >
-        &times;
-      </button>
-    </li>
-  );
+            <span
+                // style={{ color: "red" }}
+                className="todo"
+            >
+                {text}
+            </span>
+
+            <button
+                type="button"
+                onClick={() => dispatch(deleteTodo({ id }))}
+                style={{ color: "red", marginLeft: "5px", cursor: "pointer" }}
+            >
+                &times;
+            </button>
+        </li>
+    );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import TodoItem from './TodoItem';
+import React from "react";
+import { useSelector } from "react-redux";
+import TodoItem from "./TodoItem";
 
 export default function TodoList() {
-  const todos = useSelector((state) => state.todos.todos);
-  return (
-    <ul>
-      {todos.map((el) => (
-        <TodoItem
-          key={el.id}
-          {...el}
-        />
-      ))}
-
-    </ul>
-  );
+    const todos = useSelector((state) => state.todos.todos);
+    return (
+        <ul>
+            {todos.map((el) => (
+                <TodoItem key={el.id} {...el} />
+            ))}
+        </ul>
+    );
 }

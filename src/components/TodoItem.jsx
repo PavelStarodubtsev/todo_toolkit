854f5d359b770c1f63f2cea6cd4fd129a82084f2
PavelStar <pvstarodubtsev@gmail.com>
2024-06-02 19:42:01 +0300

    add app.css file

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteTodo, switchTodoComplete } from '../Redux/slices/todoSlice';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteTodo, switchTodoComplete } from "../Redux/slices/todoSlice";
+import "./style.css";
 
-export default function TodoItem({
-  id, text, complited,
-}) {
-  const dispatch = useDispatch();
+export default function TodoItem({ id, text, complited }) {
+    const dispatch = useDispatch();
 
-  return (
-    <li>
-      <input type="checkbox" checked={complited} onChange={() => dispatch(switchTodoComplete({ id }))} />
-      <span>{text}</span>
+    return (
+        <li>
+            <input
+                type="checkbox"
+                checked={complited}
+                onChange={() => dispatch(switchTodoComplete({ id }))}
+            />
+            <span className="todo">{text}</span>
 
-      <button
-        type="button"
-        onClick={() => dispatch(deleteTodo({ id }))}
-        style={{ color: 'red', marginLeft: '5px', cursor: 'pointer' }}
-      >
-        &times;
-      </button>
-    </li>
-  );
+            <button
+                className="checkbox"
+                type="button"
+                onClick={() => dispatch(deleteTodo({ id }))}
+                style={{ color: "red", marginLeft: "5px", cursor: "pointer" }}
+            >
+                &times;
+            </button>
+        </li>
+    );
 }
